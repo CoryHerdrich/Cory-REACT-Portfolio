53fdf7016092de542e7d535fca8ad363f8379a57
Cory Herdrich <128417591+CoryHerdrich@users.noreply.github.com>
2023-05-12 15:33:52 -0400

    container item

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import moment from "moment";
 
+import PortfolioContainer from './portfolio-container'
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Cory Herdrich REACT Portfolio</h1>
-        <h2>React Redux Router</h2>
-        <div>
-        {moment().format('MMMM Do YYYY, h:mm:ss a')};
-        </div>
+        <h1>Started May 11th 2023</h1>
+        <PortfolioContainer />
+        <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
       </div>
     );
   }
